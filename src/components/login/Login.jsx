42a7ea656ade6e3801478406ab492fe0ddf43dbc
nachoogomez <nachogomez09@gmail.com>
2024-11-27 10:56:22 -0300

    error msg password

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -68,6 +68,7 @@ const Login = () => {
                             required 
                             className="block w-full px-4 py-2 text-gray-700 bg-white border rounded-lg focus:border-blue-400 focus:ring-opacity-40 focus:outline-none focus:ring focus:ring-blue-300"
                         />
+                        <ErrorMessage name="password" component="div" className="text-red-500 text-sm mt-1" />
                     </div>
                     <div className="mt-6">
                         <button className="w-full px-6 py-3 text-sm font-medium tracking-wide text-white capitalize transition-colors duration-300 transform bg-gray-900 rounded-lg hover:bg-gray-700 focus:outline-none focus:ring focus:ring-gray-300 focus:ring-opacity-50">
